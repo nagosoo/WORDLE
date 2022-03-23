4121e61108eea1e15f6eb55f6cd513fd4ffc9de1
parkeunji <jjjj584@doubled.kr>
2022-03-24 00:10:40 +0900

    no message

--- app/src/main/java/com/example/wordle/Constant.kt
+++
@@ -1,8 +0,0 @@
-package com.example.wordle
-
-import android.os.Build
-
-object Constant {
-    val BASE_URL = BuildConfig.baseURL
-    val API_KEY = BuildConfig.apiKey
-}
--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -2,19 +2,12 @@ package com.example.wordle
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.wordle.retrofit.RestClient
-import com.example.wordle.retrofit.RetrofitService
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        GlobalScope.launch {RestClient().service.getTestWords() }
-
 
 
     }
